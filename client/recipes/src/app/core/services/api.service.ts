9aa460fa27c69237ed3d923391950d67d2088edb
NikolVasileva <nikol.vasileva9@gmail.com>
2026-03-29 17:54:40 +0300

    create api service

---
+++ client/recipes/src/app/core/services/api.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Recipe } from "../../shared/interfaces/recipes/recipes";
+
+@Injectable({
+    providedIn: "root"
+})
+
+export class ApiService {
+    private apiUrl = "http://localhost:3030/data";
+
+    constructor(private http: HttpClient) {}
+
+    getAllRecipes(): Observable<Recipe[]> {
+        return this.http.get<Recipe[]>(`${this.apiUrl}/recipes`)
+    }
+
+    getLatestRecipes(): Observable<Recipe[]> {
+        return this.http.get<Recipe[]>(`${this.apiUrl}/recipes?limit=4`)
+    }
+}
