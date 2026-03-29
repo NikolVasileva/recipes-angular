80034cddcdc967073bef51ee0d82648d19e3b971
NikolVasileva <nikol.vasileva9@gmail.com>
2026-03-29 17:54:09 +0300

    create recipes list

---
+++ client/recipes/src/app/features/recipes-list/recipes-list/recipes-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-list',
+  imports: [],
+  templateUrl: './recipes-list.html',
+  styleUrl: './recipes-list.css',
+})
+export class RecipesList {}
