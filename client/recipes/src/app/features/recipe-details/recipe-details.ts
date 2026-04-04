f48c0b473a8caaaa2e0a63df40b02f64a045f96a
NikolVasileva <nikol.vasileva9@gmail.com>
2026-04-04 19:01:44 +0300

    update recipe details component

--- client/recipes/src/app/features/recipe-details/recipe-details.ts
+++ client/recipes/src/app/features/recipe-details/recipe-details.ts
@@ -1,33 +1,3 @@
-// import { Component, inject, OnInit } from '@angular/core';
-// import { ActivatedRoute } from '@angular/router';
-// import { ApiService } from '../../core/services/api.service';
-// import { Recipe } from '../../shared/interfaces/recipes';
-
-// @Component({
-//   selector: 'app-recipe-details',
-//   imports: [],
-//   templateUrl: './recipe-details.html',
-//   styleUrl: './recipe-details.css',
-// })
-// export class RecipeDetails implements OnInit {
-//   private route  = inject(ActivatedRoute);
-//   private apiService = inject(ApiService);
-
-//   recipe: Recipe | null = null;
-//   recipeId = "";
-
-//   ngOnInit(): void {
-//     this.recipeId = this.route.snapshot.params["recipeId"];
-//     this.recipeDetails()
-//   };
-
-//   recipeDetails(): void {
-//     recipe$ = this.route.params.pipe(
-//       switchMap(params => this.apiService.getRecipeById(params['recipeId']))
-//     );
-//   }
-// }
-
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, switchMap } from 'rxjs';
