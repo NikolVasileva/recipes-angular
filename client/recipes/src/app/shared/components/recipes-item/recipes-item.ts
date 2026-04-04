b10b1fb88dcf1fd03382b696c9d0d759d7d1cbfb
NikolVasileva <nikol.vasileva9@gmail.com>
2026-04-04 18:02:56 +0300

    Add router link to recipe item component

--- client/recipes/src/app/shared/components/recipes-item/recipes-item.ts
+++ client/recipes/src/app/shared/components/recipes-item/recipes-item.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Recipe } from '../../interfaces/recipes';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recipes-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './recipes-item.html',
   styleUrl: './recipes-item.css',
 })
