89307cc13b0daf79f20f3937a51d3dc963a79883
NikolVasileva <nikol.vasileva9@gmail.com>
2026-04-04 16:53:39 +0300

    Show all recipes on the page

--- client/recipes/src/app/features/recipes-list/recipes-list.ts
+++ client/recipes/src/app/features/recipes-list/recipes-list.ts
@@ -1,9 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RecipesItem } from '../../shared/components/recipes-item/recipes-item';
+import { Observable } from 'rxjs';
+import { Recipe } from '../../shared/interfaces/recipes';
+import { ApiService } from '../../core/services/api.service';
 
 @Component({
   selector: 'app-recipes-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RecipesItem],
   templateUrl: './recipes-list.html',
   styleUrl: './recipes-list.css',
 })
-export class RecipesList {}
+export class RecipesList {
+  recipes$!: Observable<Recipe[]>;
+
+  constructor(private apiService: ApiService) {
+    this.recipes$ = this.apiService.getAllRecipes();
+  }
+}
