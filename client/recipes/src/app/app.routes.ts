824de16c1ac178441256d481e167b2fc523254ea
NikolVasileva <nikol.vasileva9@gmail.com>
2026-04-04 16:41:18 +0300

    fix bug with router logic

--- client/recipes/src/app/app.routes.ts
+++ client/recipes/src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { RecipesList } from './features/recipes-list/recipes-list';
 
 export const routes: Routes = [
     { path: "", redirectTo: "home", pathMatch: "full" },
-    
+
     { path: "home", component: Home },
+
     { path: "recipes", component: RecipesList }
 ];
