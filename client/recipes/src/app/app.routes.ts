0c880f09c94dfbcaf5d42568dbe7e929804aeab6
NikolVasileva <nikol.vasileva9@gmail.com>
2026-04-04 17:56:51 +0300

    Add Recipe Details to the route

--- client/recipes/src/app/app.routes.ts
+++ client/recipes/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { RecipesList } from './features/recipes-list/recipes-list';
+import { RecipeDetails } from './features/recipe-details/recipe-details';
 
 export const routes: Routes = [
     { path: "", redirectTo: "home", pathMatch: "full" },
 
     { path: "home", component: Home },
 
-    { path: "recipes", component: RecipesList }
+    { path: "recipes", component: RecipesList },
+    { path: "recipes/:recipeId", component: RecipeDetails}
 ];
