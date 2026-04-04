75b5bd827f5d3f4d0246f4eef22140e6e98e9044
NikolVasileva <nikol.vasileva9@gmail.com>
2026-04-04 16:28:53 +0300

    Add route logic

--- client/recipes/src/app/app.routes.ts
+++ client/recipes/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { RecipesList } from './features/recipes-list/recipes-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    
+    { path: "home", component: Home },
+    { path: "recipes", component: RecipesList }
+];
--- client/recipes/src/app/app.ts
+++ client/recipes/src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
-import { Home } from './features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Home, Footer],
+  imports: [Header, RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
