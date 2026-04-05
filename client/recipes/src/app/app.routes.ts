4b782539c48b905e0270754d8aa6e6dc20481cb3
NikolVasileva <nikol.vasileva9@gmail.com>
2026-04-05 17:31:16 +0300

    Show create a recipe static form

--- client/recipes/src/app/app.routes.ts
+++ client/recipes/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { RecipesList } from './features/recipes-list/recipes-list';
 import { RecipeDetails } from './features/recipe-details/recipe-details';
+import { RecipeCreate } from './features/recipe-create/recipe-create';
 
 export const routes: Routes = [
     { path: "", redirectTo: "home", pathMatch: "full" },
@@ -10,5 +11,5 @@ export const routes: Routes = [
 
     { path: "recipes", component: RecipesList },
     { path: "recipes/:recipeId", component: RecipeDetails},
-    { path: "recipe-create", component: RecipeDetails}
+    { path: "recipe-create", component: RecipeCreate}
 ];
--- client/recipes/src/app/features/recipe-create/recipe-create.ts
+++ client/recipes/src/app/features/recipe-create/recipe-create.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-create',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './recipe-create.html',
   styleUrl: './recipe-create.css',
 })
