232975b53a81f63a2e0d7bd6aef7012aefa3601e
NikolVasileva <nikol.vasileva9@gmail.com>
2026-04-06 20:27:38 +0300

    Create two directives - email validator and input error

---
+++ client/recipes/src/app/shared/directives/email-validator.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appEmailValidator]',
+})
+export class EmailValidator {
+  constructor() {}
+}
---
+++ client/recipes/src/app/shared/directives/input-error.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appInputError]',
+})
+export class InputError {
+  constructor() {}
+}
