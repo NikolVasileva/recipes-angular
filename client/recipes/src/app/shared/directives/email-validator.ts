fd01f5a3037d6f12856bb4d90a61da9f69a09761
NikolVasileva <nikol.vasileva9@gmail.com>
2026-04-06 20:45:00 +0300

    create a logig for email validator

--- client/recipes/src/app/shared/directives/email-validator.ts
+++ client/recipes/src/app/shared/directives/email-validator.ts
@@ -1,8 +1,31 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
   selector: '[appEmailValidator]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: EmailValidator,
+      multi: true,
+    }
+  ]
 })
-export class EmailValidator {
-  constructor() {}
+export class EmailValidator implements Validator {
+  validate(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+
+    if(!value) {
+      return null;
+    }
+
+    const emailRegex = /^[A-Za-z0-9._%+-]{4,}@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+    if(!emailRegex.test(value)) {
+      return { invalidEmail: true }
+    }
+
+    return null
+  }
+ 
 }
--- client/recipes/src/app/shared/directives/input-error.ts
+++ client/recipes/src/app/shared/directives/input-error.ts
@@ -2,6 +2,7 @@ import { Directive } from '@angular/core';
 
 @Directive({
   selector: '[appInputError]',
+
 })
 export class InputError {
   constructor() {}
