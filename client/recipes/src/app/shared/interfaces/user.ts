79498d03762200b98bd041578a397c7b196ae8d7
NikolVasileva <nikol.vasileva9@gmail.com>
2026-04-10 14:09:26 +0300

    create user interface

---
+++ client/recipes/src/app/shared/interfaces/user.ts
@@ -0,0 +1,16 @@
+export interface User {
+    _id: string;
+    email: string,
+    recipes?: string[],
+    created_at?: string;
+}
+
+export interface UserAuth {
+    email: string;
+    password: string;
+}
+
+export interface LoginData {
+    email: string;
+    password: string;
+}
