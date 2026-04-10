278076c03a44fa5857eee0a2ba844050b741de26
NikolVasileva <nikol.vasileva9@gmail.com>
2026-04-10 18:35:52 +0300

    create dynamic header for users

--- client/recipes/src/app/layout/header/header.ts
+++ client/recipes/src/app/layout/header/header.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterLink, Router, RouterLinkActive } from "@angular/router";
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,22 @@ import { RouterLink, Router, RouterLinkActive } from "@angular/router";
   styleUrl: './header.css',
 })
 export class Header {
-  // private router = inject(Router);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  isLoggedIn = this.authService.isLoggedIn;
+  email = computed(() => this.authService.currentUser()?.email ?? '');
+
+  onLogout(): void {
+    this.authService.logout().subscribe({
+      next: () => {
+        this.authService.clearSession();
+        this.router.navigate(['/home']);
+      },
+      error: () => {
+        this.authService.clearSession();
+        this.router.navigate(['/home']);
+      }
+    });
+  }
 }
