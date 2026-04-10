d1b5cef35cfb4deab8e736dc806962c8300060ec
NikolVasileva <nikol.vasileva9@gmail.com>
2026-04-10 14:03:53 +0300

    Create Auth Service

--- client/recipes/src/app/core/services/auth.service.ts
+++ client/recipes/src/app/core/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
-import { inject, Injectable, signal } from "@angular/core";
+import { computed, inject, Injectable, signal } from "@angular/core";
+import { LoginData, User, UserAuth } from "../../shared/interfaces/user";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: "root"
@@ -9,5 +11,29 @@ export class AuthService {
     private http = inject(HttpClient);
     private apiUrl = "http://localhost:3030/users";
 
-    private user = signal<User | null>(null)
+    private user = signal<User | null>(null);
+
+    isLoggedIn = computed(() => this.user() !== null);
+    currentUser = computed(() => this.user());
+
+    login(credentials: LoginData): Observable<User> {
+        return this.http.post<User>(`${this.apiUrl}/login`, credentials, { withCredentials: true });
+    };
+
+    register(userData: UserAuth): Observable<User> {
+        return this.http.post<User>(`${this.apiUrl}/register`, userData, { withCredentials: true });
+    };
+
+    logout(): Observable<void> {
+        return this.http.post<void>(`${this.apiUrl}/logout`, {}, { withCredentials: true });
+    };
+
+    setSession(user: User): void {
+        this.user.set(user);
+    }
+    
+    clearSession(): void {
+        this.user.set(null);
+    }
+    
 }
